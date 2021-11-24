b5888ea6788fbac1b57495efe6d3a108baf55ebe
Dmitriy <maelstrom@mail.ru>
2021-11-24 10:00:26 +0400

    Доработка async

--- index.php
+++ index.php
@@ -25,27 +25,22 @@
 <script>
     let page = 0;
     
-    function mag(){
-        (async () => {
-            let response = await fetch("http://gb.dv/requests.php?page=" + page + ",3");
-            let goods = await response.json();
-            let out = '';
-            for (let key in goods) {
-                out+=`
-                    <div class="card">
-                    <a href="/detail.php?id=`+ goods[key]['id'] +`">
-                        <h2>`+ goods[key]['title'] +`</h2>
-                        <div class="img-box"><img class="card-img" src="`+ goods[key]['url'] +`"></div>
-                    </a>
-                    <div class="price">`+ goods[key]['price'] +` &#8381;</div>
-                    <div class="description">`+ goods[key]['description'] +`</div>
-                    <div class="btn"><a href="/detail.php?id=`+ goods[key]['id'] +`">Подробнее ...</a></div>
-                </div>`
-                ;
-            }
-            let shop = document.getElementsByClassName('cards-box')[0];
-            shop.innerHTML += out;
-        })()
+    async function mag(){
+        let response = await fetch("http://gb.dv/requests.php?page=" + page + ",3");
+        let goods = await response.json();
+        let shop = document.getElementsByClassName('cards-box')[0];
+        for (let key in goods) {
+            shop.innerHTML +=`
+                <div class="card">
+                <a href="/detail.php?id=`+ goods[key]['id'] +`">
+                    <h2>`+ goods[key]['title'] +`</h2>
+                    <div class="img-box"><img class="card-img" src="`+ goods[key]['url'] +`"></div>
+                </a>
+                <div class="price">`+ goods[key]['price'] +` &#8381;</div>
+                <div class="description">`+ goods[key]['description'] +`</div>
+                <div class="btn"><a href="/detail.php?id=`+ goods[key]['id'] +`">Подробнее ...</a></div>
+            </div>`;
+        }
     }
 
     mag();
